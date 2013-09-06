85cea6cc37e3dda97e57aa6ff2066ca200447711
kimxoxo <kimxoxo@aeiou.pt>
2013-09-06 17:28:45 -0400

    changes

---
+++ baway/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Baway::Application.config.secret_token = '888d2ce01aad886293ca4afd4db7c29b691e09c970f7f90358d06366fd1b77cd47241a0eeffb4a7f6bc6c829fb3d34ddf80e11ca7fb5195c19d2b044abb48518'
