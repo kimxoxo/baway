fd93a10faae1b0c1631dd77e66ee3b5e4babcb25
kimxoxo <kimxoxo@aeiou.pt>
2014-01-30 18:16:44 -0300

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,4 +50,11 @@ namespace :deploy do
     end
   end
   before "deploy", "deploy:check_revision"
+
+
+	desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+
 end
